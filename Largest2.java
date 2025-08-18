2ddd8b0c92c8d105c20c36ec911197bd26c1ed6e
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-18 18:16:21 +0530

    18/8/25

--- Largest2.java
+++ Largest2.java
@@ -1,3 +1,16 @@
 public class Largest2 {
-    
+    public static int FindLargest(int[]arr){
+        int largest=arr[0];
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(arr[i]>largest)
+                largest=arr[i];
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int[]arr={1,-6,2,0,-2,-6,-8,1};
+        int largest_num=FindLargest(arr);
+        System.out.println("The largest number is:"+largest_num);
+    }
 }
