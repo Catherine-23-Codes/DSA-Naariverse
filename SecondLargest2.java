f83e0b803d6275fcce08453f6f1a85954fe0cc28
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-18 18:32:10 +0530

    18/8/25

--- SecondLargest2.java
+++ SecondLargest2.java
@@ -1,3 +1,21 @@
 public class SecondLargest2 {
-    
+    public static int LinearSearch(int[]arr,int target){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[]arr={1,6,2,7,3,8,4};
+        int target=7;
+        int index=LinearSearch(arr,target);
+        if(index!=-1){
+            System.out.println("The number is found at index:"+index);
+        }else{
+            System.out.println("The number is not found at all!");
+        }
+    }
 }
