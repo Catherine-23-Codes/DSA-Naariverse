3de664015359582b5bd449f580403c19a8e27277
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-22 21:28:16 +0530

    22/8/25

--- RemoveDuplicates1.java
+++ RemoveDuplicates1.java
@@ -1,3 +1,25 @@
 public class RemoveDuplicates1 {
-    
+    public static int removeDuplicates(int[] arr) {
+        if (arr.length == 0) return 0;
+
+        int j = 0; // index of last unique element
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i]; 
+            }
+        }
+        return j + 1; // number of unique elements
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int n = removeDuplicates(arr);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
+
