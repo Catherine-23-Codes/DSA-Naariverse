46b58ecd559c84a6faa26f10addd0d2b74abe06d
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-08-23 23:21:36 +0530

    22/8/25

---
+++ Large.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Large{
+    public static int FindLargest(int[]arr){
+        int n=arr.length;
+        Arrays.sort(arr);
+        return arr[n-1];
+    }
+    public static void main(String[] args) {
+        int[]arr={1,0,4,76,2,7,11,8,35};
+        int largest_num=FindLargest(arr);
+        System.out.println("The largest number is:"+largest_num);
+    }
+}
+
